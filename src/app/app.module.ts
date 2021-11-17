6e52a57b1999a76f62b75230d29f440703455a12
radeen <radeen013@gmail.com>
2021-11-17 08:57:18 -0500

    needtocomebackandfinish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DonutsComponent } from './donuts/donuts.component';
+import { DonutDetailComponent } from './donut-detail/donut-detail.component';
+import { FamousPeopleComponent } from './famous-people/famous-people.component';
+import { FamousPersonDetailComponent } from './famous-person-detail/famous-person-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DonutsComponent,
+    DonutDetailComponent,
+    FamousPeopleComponent,
+    FamousPersonDetailComponent
   ],
   imports: [
     BrowserModule,
